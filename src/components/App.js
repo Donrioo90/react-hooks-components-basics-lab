d4479deb2074a2fe0c44bafc3be0109ca0dd2ad4
Ronnie Wanjala <ranjala90@gmail.com>
2024-08-03 15:30:59 +0300

    save

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
     </nav>
   );
 }
@@ -16,15 +17,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// Write the <About> component here
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />  {/* Add the <Home> component here */}
+      <About /> {/* Add your <About> component here */}
     </div>
   );
 }
